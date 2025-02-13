35ccfa551b3d6c8ce78226b1ac44b416f1803811
CRYPTX <83192667+cryptxwastaken@users.noreply.github.com>
2025-02-13 21:34:50 +0700

    Write logic to get the human choice

--- script.js
+++ script.js
@@ -3,4 +3,19 @@ function getComputerChoice(n) {
     return Math.floor((Math.random() * n) + 1);
 }
 
-console.log(getComputerChoice(3));
+// takes the user choice and returns it
+function getHumanChoice() {
+    humanChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
+    switch (humanChoice) {
+        case "rock":
+            return 1;
+        case "paper":
+            return 2;
+        case "scissors":
+            return 3;
+        default:
+            alert("Invalid choice! Try again!");
+    }
+}
+
+console.log(getHumanChoice())
